2475bc476a867e3618eb665489e5ba74f18ce26a
David Grudl <david@grudl.com>
2008-08-24 23:13:56 +0000

    added Nette\ITranslator

---
+++ src/Utils/ITranslator.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Nette Framework
+ *
+ * Copyright (c) 2004, 2008 David Grudl (http://davidgrudl.com)
+ *
+ * This source file is subject to the "Nette license" that is bundled
+ * with this package in the file license.txt.
+ *
+ * For more information please see http://nettephp.com
+ *
+ * @copyright  Copyright (c) 2004, 2008 David Grudl
+ * @license    http://nettephp.com/license  Nette license
+ * @link       http://nettephp.com
+ * @category   Nette
+ * @package    Nette
+ * @version    $Id$
+ */
+
+/*namespace Nette;*/
+
+
+
+/**
+ * Translator adapter.
+ *
+ * @author     David Grudl
+ * @copyright  Copyright (c) 2004, 2008 David Grudl
+ * @package    Nette
+ */
+interface ITranslator
+{
+
+	/**
+	 * Translates the given string.
+	 * @param  string   message
+	 * @param  int      plural
+	 * @return string
+	 */
+	function translate($message, $plural = NULL);
+
+}
