8832e3323353eb8b1571b12425bf10bbd5e58d23
David Grudl <david@grudl.com>
2008-10-05 18:36:37 +0000

    added String::normalize() & String::webalize()

--- src/Utils/String.php
+++ src/Utils/String.php
@@ -45,14 +45,11 @@ final class String
 	/**
 	 * Checks if the string is valid for the UTF-8 encoding.
 	 * @param  string
-	 * @return string
+	 * @return bool
 	 */
-	public static function checkUTF($s)
+	public static function checkUtf($s)
 	{
-		if (preg_match('##u', $s)) {
-			return $s;
-		}
-		return FALSE;
+		return (bool) preg_match('##u', $s);
 	}
 
 
@@ -79,6 +76,57 @@ final class String
 	public static function endsWith($haystack, $needle)
 	{
 		return strlen($needle) === 0 || substr($haystack, -strlen($needle)) === $needle;
+	}
+
+
+
+	/**
+	 * Removes special controls characters and normalizes line endings and spaces.
+	 * @param  string
+	 * @return string
+	 */
+	public static function normalize($s)
+	{
+		// standardize line endings to unix-like
+		$s = str_replace("\r\n", "\n", $s); // DOS
+		$s = strtr($s, "\r", "\n"); // Mac
+
+		// remove control characters; leave \t + \n
+		$s = preg_replace('#[\x00-\x08\x0B-\x1F]+#', '', $s);
+
+		// right trim
+		$s = preg_replace("#[\t ]+$#m", '', $s);
+
+		// trailing spaces
+		$s = trim($s, "\n");
+
+		return $s;
+	}
+
+
+
+	/**
+	 * Converts to web safe characters [a-z0-9-] text.
+	 * @param  string
+	 * @param  string
+	 * @return string
+	 */
+	public static function webalize($s, $charlist = NULL)
+	{
+		$s = strtr($s, '`\'"^~', '-----');
+		if (ICONV_IMPL === 'glibc') {
+			$s = iconv('UTF-8', 'WINDOWS-1250//TRANSLIT', $s);
+			$s = strtr($s, "\xa5\xa3\xbc\x8c\xa7\x8a\xaa\x8d\x8f\x8e\xaf\xb9\xb3\xbe\x9c\x9a\xba\x9d\x9f\x9e\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2"
+				."\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf8\xf9\xfa\xfb\xfc\xfd\xfe",
+				"ALLSSSSTZZZallssstzzzRAAAALCCCEEEEIIDDNNOOOOxRUUUUYTsraaaalccceeeeiiddnnooooruuuuyt");
+		} else {
+			$s = iconv('UTF-8', 'ASCII//TRANSLIT', $s);
+		}
+		$s = str_replace(array('`', "'", '"', '^', '~'), '', $s);
+		$s = strtolower($s);
+		$s = preg_replace('#[^a-z0-9'.preg_quote($charlist, '#').']+#', '-', $s);
+		$s = trim($s, '-');
+		return $s;
 	}
 
 }
