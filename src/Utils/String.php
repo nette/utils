5b041dbcd13ee6f6c9cd4a26ffd2efaa17e527c8
David Grudl <david@grudl.com>
2008-12-31 00:28:33 +0000

    year 2008 -> 2009

--- src/Utils/String.php
+++ src/Utils/String.php
@@ -3,14 +3,14 @@
 /**
  * Nette Framework
  *
- * Copyright (c) 2004, 2008 David Grudl (http://davidgrudl.com)
+ * Copyright (c) 2004, 2009 David Grudl (http://davidgrudl.com)
  *
  * This source file is subject to the "Nette license" that is bundled
  * with this package in the file license.txt.
  *
  * For more information please see http://nettephp.com
  *
- * @copyright  Copyright (c) 2004, 2008 David Grudl
+ * @copyright  Copyright (c) 2004, 2009 David Grudl
  * @license    http://nettephp.com/license  Nette license
  * @link       http://nettephp.com
  * @category   Nette
@@ -26,7 +26,7 @@
  * String tools library.
  *
  * @author     David Grudl
- * @copyright  Copyright (c) 2004, 2008 David Grudl
+ * @copyright  Copyright (c) 2004, 2009 David Grudl
  * @package    Nette
  */
 final class String
