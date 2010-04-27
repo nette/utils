f330ab4c68bc728b6c30f290f27cac69a5514ae1
David Grudl <david@grudl.com>
2010-04-27 02:56:22 +0200

    Nette\Collections removed (except lite version of ArrayList)

---
+++ src/Utils/ArrayList.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Nette Framework
+ *
+ * @copyright  Copyright (c) 2004, 2010 David Grudl
+ * @license    http://nettephp.com/license  Nette license
+ * @link       http://nettephp.com
+ * @category   Nette
+ * @package    Nette
+ */
+
+/*namespace Nette;*/
+
+
+
+/**
+ * Provides the base class for a generic list (items can be accessed by index). In PHP 5.3 use SplQueue.
+ *
+ * @copyright  Copyright (c) 2004, 2010 David Grudl
+ * @package    Nette
+ */
+class ArrayList implements /*\*/ArrayAccess, /*\*/Countable, /*\*/IteratorAggregate
+{
+	private $list = array();
+
+
+
+	/**
+	 * Returns an iterator over all items.
+	 * @return \ArrayIterator
+	 */
+	public function getIterator()
+	{
+		return new /*\*/ArrayIterator($this->list);
+	}
+
+
+
+	/**
+	 * Returns items count.
+	 * @return int
+	 */
+	public function count()
+	{
+		return count($this->list);
+	}
+
+
+
+	/**
+	 * Replaces or appends a item.
+	 * @param  int
+	 * @param  mixed
+	 * @return void
+	 * @throws \OutOfRangeException
+	 */
+	public function offsetSet($index, $value)
+	{
+		if ($index === NULL) {
+			$this->list[] = $value;
+
+		} elseif ($index < 0 || $index >= count($this->list)) {
+			throw new /*\*/OutOfRangeException("Offset invalid or out of range");
+
+		} else {
+			$this->list[(int) $index] = $value;
+		}
+	}
+
+
+
+	/**
+	 * Returns a item.
+	 * @param  int
+	 * @return mixed
+	 * @throws \OutOfRangeException
+	 */
+	public function offsetGet($index)
+	{
+		if ($index < 0 || $index >= count($this->list)) {
+			throw new /*\*/OutOfRangeException("Offset invalid or out of range");
+		}
+		return $this->list[(int) $index];
+	}
+
+
+
+	/**
+	 * Determines whether a item exists.
+	 * @param  int
+	 * @return bool
+	 */
+	public function offsetExists($index)
+	{
+		return $index >= 0 && $index < count($this->list);
+	}
+
+
+
+	/**
+	 * Removes the element at the specified position in this list.
+	 * @param  int
+	 * @return void
+	 * @throws \OutOfRangeException
+	 */
+	public function offsetUnset($index)
+	{
+		if ($index < 0 || $index >= count($this->list)) {
+			throw new /*\*/OutOfRangeException("Offset invalid or out of range");
+		}
+		array_splice($this->list, (int) $index, 1);
+	}
+
+}
---
+++ tests/Utils/ArrayList.001.phpt
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Test: Nette\ArrayList basic usage.
+ *
+ * @author     David Grudl
+ * @category   Nette
+ * @package    Nette
+ * @subpackage UnitTests
+ */
+
+/*use Nette\ArrayList;*/
+
+
+
+require dirname(__FILE__) . '/../NetteTest/initialize.php';
+
+
+
+class Person
+{
+	private $name;
+
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+
+
+
+	public function sayHi()
+	{
+		output("My name is $this->name");
+	}
+
+
+
+	public function __toString()
+	{
+		return $this->name;
+	}
+}
+
+
+
+$list = new ArrayList;
+$jack = new Person('Jack');
+$mary = new Person('Mary');
+
+
+
+output("Adding Mary");
+$list[] = $mary;
+
+output("Adding Jack");
+$list[] = $jack;
+
+
+
+dump( $list->count(), 'count:' );
+dump( count($list) );
+
+
+dump( iterator_to_array($list) );
+
+
+
+output("Get Interator:");
+foreach ($list as $key => $person) {
+	echo $key, ' => ', $person->sayHi();
+}
+
+
+
+try {
+	output("unset -1");
+	unset($list[-1]);
+} catch (Exception $e) {
+	dump( $e );
+}
+
+try {
+	output("unset 1");
+	unset($list[1]);
+} catch (Exception $e) {
+	dump( $e );
+}
+
+dump( iterator_to_array($list) );
+
+
+
+__halt_compiler();
+
+------EXPECT------
+Adding Mary
+
+Adding Jack
+
+count: int(2)
+
+int(2)
+
+array(2) {
+	0 => object(Person) (1) {
+		"name" private => string(4) "Mary"
+	}
+	1 => object(Person) (1) {
+		"name" private => string(4) "Jack"
+	}
+}
+
+Get Interator:
+
+0 => My name is Mary
+
+1 => My name is Jack
+
+unset -1
+
+Exception OutOfRangeException: Offset invalid or out of range
+
+unset 1
+
+array(1) {
+	0 => object(Person) (1) {
+		"name" private => string(4) "Mary"
+	}
+}
