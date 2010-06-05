af1898071146b1d640a8c63f2a9ef25f4c6643c7
David Grudl <david@grudl.com>
2010-06-05 23:23:35 +0200

    String::webalize() better fix for glibc implementation

--- src/Utils/String.php
+++ src/Utils/String.php
@@ -137,13 +137,9 @@ final class String
 	{
 		$s = strtr($s, '`\'"^~', '-----');
 		if (ICONV_IMPL === 'glibc') {
-			$s = @iconv('UTF-8', 'WINDOWS-1250//TRANSLIT', $s); // intentionally @
-			$s = strtr($s, "\xa5\xa3\xbc\x8c\xa7\x8a\xaa\x8d\x8f\x8e\xaf\xb9\xb3\xbe\x9c\x9a\xba\x9d\x9f\x9e\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2"
-				."\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf8\xf9\xfa\xfb\xfc\xfd\xfe",
-				"ALLSSSSTZZZallssstzzzRAAAALCCCEEEEIIDDNNOOOOxRUUUUYTsraaaalccceeeeiiddnnooooruuuuyt");
-		} else {
-			$s = @iconv('UTF-8', 'ASCII//TRANSLIT', $s); // intentionally @
+			setlocale(LC_CTYPE, 'en_US.UTF-8');
 		}
+		$s = @iconv('UTF-8', 'ASCII//TRANSLIT', $s); // intentionally @
 		$s = str_replace(array('`', "'", '"', '^', '~'), '', $s);
 		if ($lower) $s = strtolower($s);
 		$s = preg_replace('#[^a-z0-9' . preg_quote($charlist, '#') . ']+#i', '-', $s);
