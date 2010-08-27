dc60202bed3f218dbfe8ee2decc8a380e3f5dc68
David Grudl <david@grudl.com>
2010-08-27 17:50:46 +0200

    added String::toAscii()

--- src/Utils/String.php
+++ src/Utils/String.php
@@ -127,15 +127,14 @@ final class String
 
 
 	/**
-	 * Converts to web safe characters [a-z0-9-] text.
+	 * Converts to ASCII.
 	 * @param  string  UTF-8 encoding
-	 * @param  string  ASCII
-	 * @param  bool
-	 * @return string
+	 * @return string  ASCII
 	 */
-	public static function webalize($s, $charlist = NULL, $lower = TRUE)
+	public static function toAscii($s)
 	{
-		$s = strtr($s, '`\'"^~', '-----');
+		$s = preg_replace('#[^\x09\x0A\x0D\x20-\x7E\xA0-\x{10FFFF}]#u', '', $s);
+		$s = strtr($s, '`\'"^~', "\x01\x02\x03\x04\x05");
 		if (ICONV_IMPL === 'glibc') {
 			$s = @iconv('UTF-8', 'WINDOWS-1250//TRANSLIT', $s); // intentionally @
 			$s = strtr($s, "\xa5\xa3\xbc\x8c\xa7\x8a\xaa\x8d\x8f\x8e\xaf\xb9\xb3\xbe\x9c\x9a\xba\x9d\x9f\x9e\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2"
@@ -145,6 +144,21 @@ final class String
 			$s = @iconv('UTF-8', 'ASCII//TRANSLIT', $s); // intentionally @
 		}
 		$s = str_replace(array('`', "'", '"', '^', '~'), '', $s);
+		return strtr($s, "\x01\x02\x03\x04\x05", '`\'"^~');
+	}
+
+
+
+	/**
+	 * Converts to web safe characters [a-z0-9-] text.
+	 * @param  string  UTF-8 encoding
+	 * @param  string  allowed characters
+	 * @param  bool
+	 * @return string
+	 */
+	public static function webalize($s, $charlist = NULL, $lower = TRUE)
+	{
+		$s = self::toAscii($s);
 		if ($lower) $s = strtolower($s);
 		$s = preg_replace('#[^a-z0-9' . preg_quote($charlist, '#') . ']+#i', '-', $s);
 		$s = trim($s, '-');
---
+++ tests/Utils/String.toAscii().phpt
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Test: Nette\String::toAscii()
+ *
+ * @author     David Grudl
+ * @category   Nette
+ * @package    Nette
+ * @subpackage UnitTests
+ */
+
+use Nette\String;
+
+
+
+require __DIR__ . '/../initialize.php';
+
+
+
+Assert::same( 'ZLUTOUCKY KUN oooo', String::toAscii("\xc5\xbdLU\xc5\xa4OU\xc4\x8cK\xc3\x9d K\xc5\xae\xc5\x87 \xc3\xb6\xc5\x91\xc3\xb4o") ); // ŽLUŤOUČKÝ KŮŇ öőôo
+Assert::same( 'Z `\'"^~', String::toAscii("\xc5\xbd `'\"^~") );
