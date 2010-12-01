218db0bedc7df4a98b1e497a5f0c95974707dead
David Grudl <david@grudl.com>
2010-12-01 17:31:32 +0100

    added MapIterator

---
+++ src/Iterators/MapIterator.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework.
+ *
+ * Copyright (c) 2004, 2010 David Grudl (http://davidgrudl.com)
+ *
+ * This source file is subject to the "Nette license", and/or
+ * GPL license. For more information please see http://nette.org
+ */
+
+namespace Nette;
+
+use Nette;
+
+
+
+/**
+ * Applies the callback to the elements of the inner iterator.
+ *
+ * @author     David Grudl
+ */
+class MapIterator extends \IteratorIterator
+{
+	/** @var callback */
+	private $callback;
+
+
+	/**
+	 * Constructs a filter around another iterator.
+	 * @param
+	 * @param  callback
+	 */
+	function __construct(\Traversable $iterator, $callback)
+	{
+		parent::__construct($iterator);
+		$this->callback = $callback;
+	}
+
+
+
+	function current()
+	{
+		return call_user_func($this->callback, parent::current(), parent::key());
+	}
+
+}
