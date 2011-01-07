e779a9dceeed02f27b8fa79727c9a9caa3f179c7
David Grudl <david@grudl.com>
2011-01-07 15:05:01 +0100

    year 2010 -> 2011 (this is absolutely useless commit, but I like it)

--- src/Utils/String.php
+++ src/Utils/String.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Nette Framework.
  *
- * Copyright (c) 2004, 2010 David Grudl (http://davidgrudl.com)
+ * Copyright (c) 2004, 2011 David Grudl (http://davidgrudl.com)
  *
  * This source file is subject to the "Nette license", and/or
  * GPL license. For more information please see http://nette.org
@@ -323,7 +323,7 @@ final class String
 	/**
 	 * Generate random string.
 	 * @param  int
-	 * @return string
+	 * @param  string
 	 * @return string
 	 */
 	public static function random($length = 10, $charlist = '0-9a-z')
--- src/Utils/Tools.php
+++ src/Utils/Tools.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Nette Framework.
  *
- * Copyright (c) 2004, 2010 David Grudl (http://davidgrudl.com)
+ * Copyright (c) 2004, 2011 David Grudl (http://davidgrudl.com)
  *
  * This source file is subject to the "Nette license", and/or
  * GPL license. For more information please see http://nette.org
