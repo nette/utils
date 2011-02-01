43d2c23828f7522f79bb92ce9c7a4bcad3623fff
David Grudl <david@grudl.com>
2011-02-02 00:38:23 +0100

    License changed to the New BSD License or the GNU General Public License (GPL) version 2 or 3.

--- src/Iterators/CallbackFilterIterator.php
+++ src/Iterators/CallbackFilterIterator.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * This file is part of the Nette Framework.
+ * This file is part of the Nette Framework (http://nette.org)
  *
  * Copyright (c) 2004, 2011 David Grudl (http://davidgrudl.com)
  *
- * This source file is subject to the "Nette license", and/or
- * GPL license. For more information please see http://nette.org
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
  */
 
 namespace Nette;
