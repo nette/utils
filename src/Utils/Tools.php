5214198db759a71dd915ed6b221d0821051e20c0
David Grudl <david@grudl.com>
2011-02-01 22:14:01 +0100

    lockfile is created only on Windows platform

--- src/Utils/Tools.php
+++ src/Utils/Tools.php
@@ -200,9 +200,10 @@ final class Tools
 		if (self::$criticalSections) {
 			throw new \InvalidStateException('Critical section has already been entered.');
 		}
-		$handle = @fopen(NETTE_DIR . '/lockfile', 'r') ?: fopen(NETTE_DIR . '/lockfile', 'w'); // @ - file may not already exist
+		// locking on Windows causes that a file seems to be empty
+		$handle = substr(PHP_OS, 0, 3) === 'WIN' ? @fopen(NETTE_DIR . '/lockfile', 'w') : @fopen(__FILE__, 'r'); // @ - file may not already exist
 		if (!$handle) {
-			throw new \InvalidStateException("Unable initialize critical section (missing file '" . NETTE_DIR . "/lockfile').");
+			throw new \InvalidStateException("Unable initialize critical section.");
 		}
 		flock(self::$criticalSections = $handle, LOCK_EX);
 	}
