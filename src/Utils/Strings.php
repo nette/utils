469e0b1ff3822efe4f20cf5ff425574f04eb78db
David Grudl <david@grudl.com>
2011-07-01 02:23:38 +0200

    Strings: added expand() (from DI\Container)

--- src/Utils/Strings.php
+++ src/Utils/Strings.php
@@ -483,6 +483,51 @@ class Strings
 		}
 	}
 
+
+
+	/**
+	 * Expands %placeholders% in string.
+	 * @param  mixed
+	 * @param  array
+	 * @param  bool
+	 * @return mixed
+	 * @throws Nette\InvalidArgumentException
+	 */
+	public static function expand($s, array $params, $recursive = FALSE)
+	{
+		if (!is_string($s) || strpos($s, '%') === FALSE) {
+			return $s;
+		}
+
+		$parts = preg_split('#%([a-z0-9._-]*)%#i', $s, -1, PREG_SPLIT_DELIM_CAPTURE);
+		$res = '';
+		foreach ($parts as $n => $part) {
+			if ($n % 2 === 0) {
+				$res .= $part;
+
+			} elseif ($part === '') {
+				$res .= '%';
+
+			} elseif (isset($recursive[$part])) {
+				throw new Nette\InvalidArgumentException('Circular reference detected for variables: ' . implode(', ', array_keys($recursive)) . '.');
+
+			} else {
+				$val = Arrays::get($params, explode('.', $part));
+				if ($recursive) {
+					$val = Strings::expand($val, $params, (is_array($recursive) ? $recursive : array()) + array($part => 1));
+				}
+				if (strlen($part) + 2 === strlen($s)) {
+					return $val;
+				}
+				if (!is_scalar($val)) {
+					throw new Nette\InvalidArgumentException("Unable to concatenate non-scalar parameter '$part' into '$s'.");
+				}
+				$res .= $val;
+			}
+		}
+		return $res;
+	}
+
 }
 
 
