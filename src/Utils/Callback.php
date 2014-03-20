e18685d880971b6eef676c77bae13c274374188d
David Grudl <david@grudl.com>
2014-03-20 22:56:09 +0100

    added Nette\Utils\Callback, static replacement of Nette\Callback
    Object Nette\Callback can be replaced with native Closure.

---
+++ src/Utils/Callback.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (http://nette.org)
+ *
+ * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+
+namespace Nette\Utils;
+
+use Nette;
+
+
+/**
+ * PHP callable tools.
+ *
+ * @author     David Grudl
+ */
+final class Callback
+{
+
+	/**
+	 * @param  mixed   class, object, callable
+	 * @param  string  method
+	 * @return \Closure
+	 */
+	public static function closure($callable, $m = NULL)
+	{
+		if ($m !== NULL) {
+			$callable = array($callable, $m);
+		} elseif ($callable instanceof \Closure) {
+			return $callable;
+		}
+
+		self::check($callable, TRUE);
+		$_callable_ = $callable;
+		return function() use ($_callable_) {
+			Callback::check($_callable_);
+			return call_user_func_array($_callable_, func_get_args());
+		};
+	}
+
+
+	/**
+	 * Invokes callback.
+	 * @return mixed
+	 */
+	public static function invoke($callable)
+	{
+		self::check($callable);
+		return call_user_func_array($callable, array_slice(func_get_args(), 1));
+	}
+
+
+	/**
+	 * Invokes callback with an array of parameters.
+	 * @return mixed
+	 */
+	public static function invokeArgs($callable, array $args = array())
+	{
+		self::check($callable);
+		return call_user_func_array($callable, $args);
+	}
+
+
+	/**
+	 * @return callable
+	 */
+	public static function check($callable, $syntax = FALSE)
+	{
+		if (!is_callable($callable, $syntax)) {
+			throw new Nette\InvalidArgumentException($syntax
+				? 'Given value is not a callable type.'
+				: "Callback '" . self::toString($callable) . "' is not callable."
+			);
+		}
+		return $callable;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public static function toString($callable)
+	{
+		if ($callable instanceof \Closure) {
+			if ($inner = self::unwrap($callable)) {
+				return '{closure ' . self::toString($inner) . '}';
+			}
+			return '{closure}';
+		} elseif (is_string($callable) && $callable[0] === "\0") {
+			return '{lambda}';
+		} else {
+			is_callable($callable, TRUE, $textual);
+			return $textual;
+		}
+	}
+
+
+	/**
+	 * @return Nette\Reflection\GlobalFunction|Nette\Reflection\Method
+	 */
+	public static function toReflection($callable)
+	{
+		if ($callable instanceof \Closure && $inner = self::unwrap($callable)) {
+			$callable = $inner;
+		} elseif ($callable instanceof Nette\Callback) {
+			$callable = $callable->getNative();
+		}
+
+		if (is_string($callable) && strpos($callable, '::')) {
+			return new Nette\Reflection\Method($callable);
+		} elseif (is_array($callable)) {
+			return new Nette\Reflection\Method($callable[0], $callable[1]);
+		} elseif (is_object($callable) && !$callable instanceof \Closure) {
+			return new Nette\Reflection\Method($callable, '__invoke');
+		} else {
+			return new Nette\Reflection\GlobalFunction($callable);
+		}
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public static function isStatic($callable)
+	{
+		return is_array($callable) ? is_string($callable[0]) : is_string($callable);
+	}
+
+
+
+	/**
+	 * Unwraps closure created by self::closure(), used i.e. by ObjectMixin in PHP < 5.4
+	 * @internal
+	 * @return callable
+	 */
+	public static function unwrap(\Closure $closure)
+	{
+		$rm = new \ReflectionFunction($closure);
+		$vars = $rm->getStaticVariables();
+		return isset($vars['_callable_']) ? $vars['_callable_'] : NULL;
+	}
+
+}
---
+++ tests/Utils/Callback.phpt
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Test: Nette\Utils\Callback tests.
+ *
+ * @author     David Grudl
+ * @package    Nette
+ */
+
+use Nette\Utils\Callback;
+
+
+require __DIR__ . '/../bootstrap.php';
+
+
+class Test
+{
+	static function add($a, $b)
+	{
+		return $a + $b;
+	}
+
+	function __invoke()
+	{
+	}
+
+}
+
+test(function() {
+	Assert::same( 'undefined', Callback::unwrap(Callback::closure('undefined')) );
+	Assert::same( 'undefined', Callback::toString('undefined') );
+	Assert::exception(function() {
+		Callback::toReflection('undefined');
+	}, 'ReflectionException');
+
+	Assert::same( 'trim', Callback::unwrap(Callback::closure('trim')) );
+	Assert::same( 'trim', Callback::toString('trim') );
+	Assert::same( 'trim()', (string) Callback::toReflection('trim') );
+
+	Assert::same( array('Test', 'add'), Callback::unwrap(Callback::closure('Test', 'add')) );
+	Assert::same( 'Test::add', Callback::toString(array('Test', 'add')) );
+	Assert::same( 'Test::add()', (string) Callback::toReflection(array('Test', 'add')) );
+
+	Assert::same( 'Test::add', Callback::unwrap(Callback::closure('Test::add')) );
+	Assert::same( 'Test::add', Callback::toString('Test::add') );
+	Assert::same( 'Test::add()', (string) Callback::toReflection('Test::add') );
+
+	$test = new Test;
+	Assert::same( array($test, 'add'), Callback::unwrap(Callback::closure($test, 'add')) );
+	Assert::same( 'Test::add', Callback::toString(array($test, 'add')) );
+	Assert::same( 'Test::add()', (string) Callback::toReflection(array($test, 'add')) );
+
+	Assert::same( $test, Callback::unwrap(Callback::closure($test)) );
+	Assert::same( 'Test::__invoke', Callback::toString($test) );
+	Assert::same( 'Test::__invoke()', (string) Callback::toReflection($test) );
+
+	$closure = function(){};
+	Assert::same( $closure, Callback::closure($closure) );
+	Assert::same( '{closure}', Callback::toString($closure) );
+	Assert::same( '{closure}()', (string) Callback::toReflection($closure) );
+
+	Assert::same( '{closure Test::add}', Callback::toString(Callback::closure($test, 'add')) );
+	Assert::same( 'Test::add()', (string) Callback::toReflection(Callback::closure($test, 'add')) );
+});
+
+
+test(function() {
+	$cb = array(new Test, 'add');
+
+	Assert::same( 8, Callback::invoke($cb, 3, 5) );
+	Assert::same( 8, Callback::invokeArgs($cb, array(3, 5)) );
+
+	Assert::exception(function() {
+		Callback::invoke('undefined');
+	}, 'Nette\InvalidArgumentException', "Callback 'undefined' is not callable.");
+
+	Assert::exception(function() {
+		Callback::invokeArgs('undefined');
+	}, 'Nette\InvalidArgumentException', "Callback 'undefined' is not callable.");
+});
+
+
+test(function() {
+	$cb = array(new Test, 'add');
+
+	Assert::same( $cb, Callback::check($cb) );
+	Callback::check('undefined', TRUE);
+
+	Assert::exception(function() {
+		Callback::check(123, TRUE);
+	}, 'Nette\InvalidArgumentException', 'Given value is not a callable type.');
+
+	Assert::exception(function() {
+		Callback::check('undefined');
+	}, 'Nette\InvalidArgumentException', "Callback 'undefined' is not callable.");
+});
