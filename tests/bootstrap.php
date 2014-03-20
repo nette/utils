b5cbbc45ef2232fde3457fb9c1c1e1b22581e4d9
David Grudl <david@grudl.com>
2014-03-20 21:39:31 +0100

    added testing support files

---
+++ tests/bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+// The Nette Tester command-line runner can be
+// invoked through the command: ../vendor/bin/tester .
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo 'Install Nette Tester using `composer update --dev`';
+	exit(1);
+}
+
+
+// configure environment
+Tester\Environment::setup();
+date_default_timezone_set('Europe/Prague');
+
+
+// create temporary directory
+define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
+@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
+Tester\Helpers::purge(TEMP_DIR);
+
+
+function before(\Closure $function = NULL)
+{
+	static $val;
+	if (!func_num_args()) {
+		return ($val ? $val() : NULL);
+	}
+	$val = $function;
+}
+
+
+function test(\Closure $function)
+{
+	before();
+	$function();
+}
