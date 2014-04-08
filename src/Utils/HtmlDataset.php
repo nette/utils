396f8a75fbdc2e07105e852ea8f2dd8467ab1063
Petr Morávek [Xificurk] <petr@pada.cz>
2014-04-08 18:09:08 +0200

    Add HtmlDataset as bridge between HTML data attributes and stored objects.

---
+++ src/Utils/HtmlDataset.php
@@ -0,0 +1,181 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (http://nette.org)
+ * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+ */
+
+namespace Nette\Utils;
+
+use Nette;
+
+
+/**
+ * HTML dataset helper.
+ *
+ * @author     Petr Morávek <petr@pada.cz>
+ */
+class HtmlDataset extends Nette\Object implements \ArrayAccess, \Countable, \IteratorAggregate, IHtmlString
+{
+	/** @var array */
+	private $data = array();
+
+
+	public function __construct($data = array())
+	{
+		foreach ($data as $name => $value) {
+			$this->data[$this->dataKey($name)] = $value;
+		}
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		$s = '';
+		foreach ($this->data as $k => $v) {
+			if ($v === NULL) {
+				continue;
+			}
+			if (!is_string($v)) {
+				$v = Json::encode($v);
+			}
+			$q = strpos($v, '"') === FALSE ? '"' : "'";
+			$v = $q . str_replace(array('&', $q), array('&amp;', $q === '"' ? '&quot;' : '&#39;'), $v) . $q;
+			$s .= ' data-' . strtolower(preg_replace('#(.)(?=[A-Z])#', '$1-', $k)) . '=' . $v;
+		}
+		return ltrim($s);
+	}
+
+
+	/**
+	 * Overloaded setter for data attribute.
+	 * @param  string    data attribute name
+	 * @param  mixed     data attribute value
+	 * @return void
+	 */
+	public function __set($name, $value)
+	{
+		$this->data[$this->dataKey($name)] = $value;
+	}
+
+
+	/**
+	 * Overloaded getter for data attribute.
+	 * @param  string    data attribute name
+	 * @return mixed     data attribute value
+	 */
+	public function &__get($name)
+	{
+		return $this->data[$this->dataKey($name)];
+	}
+
+
+	/**
+	 * Overloaded tester for data attribute.
+	 * @param  string    data attribute name
+	 * @return bool
+	 */
+	public function __isset($name)
+	{
+		return isset($this->data[$this->dataKey($name)]);
+	}
+
+
+	/**
+	 * Overloaded unsetter for data attribute.
+	 * @param  string    data attribute name
+	 * @return void
+	 */
+	public function __unset($name)
+	{
+		unset($this->data[$this->dataKey($name)]);
+	}
+
+
+	/**
+	 * Inserts (replaces) content for data attribute (\ArrayAccess implementation).
+	 * @param  string name
+	 * @param  mixed data
+	 * @return void
+	 */
+	public function offsetSet($name, $value)
+	{
+		$this->$name = $value;
+	}
+
+
+	/**
+	 * Returns content of data attribute (\ArrayAccess implementation).
+	 * @param  string name
+	 * @return mixed
+	 */
+	public function offsetGet($name)
+	{
+		return $this->$name;
+	}
+
+
+	/**
+	 * Does data attribute exist? (\ArrayAccess implementation).
+	 * @param  string name
+	 * @return bool
+	 */
+	public function offsetExists($name)
+	{
+		return isset($this->$name);
+	}
+
+
+	/**
+	 * Removes data attribute (\ArrayAccess implementation).
+	 * @param  int name
+	 * @return void
+	 */
+	public function offsetUnset($name)
+	{
+		unset($this->$name);
+	}
+
+
+	/**
+	 * Required by the \Countable interface.
+	 * @return int
+	 */
+	public function count()
+	{
+		return count($this->data);
+	}
+
+
+	/**
+	 * Returns an iterator over all items.
+	 * @return \ArrayIterator
+	 */
+	public function getIterator()
+	{
+		return new \ArrayIterator($this->data);
+	}
+
+
+	/**
+	 * Returns camelCase data key.
+	 * @param  string
+	 * @return string
+	 */
+	private function dataKey($s)
+	{
+		if (strpos($s, '-') === FALSE) {
+			return strtolower(substr($s, 0, 1)) . substr($s, 1);
+		}
+
+		$s = strtolower($s);
+		$s = preg_replace('#-(?=[a-z])#', ' ', $s);
+		$s = substr(ucwords('x' . $s), 1);
+		$s = str_replace(' ', '', $s);
+		return $s;
+	}
+
+}
---
+++ tests/Utils/HtmlDataset.phpt
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Test: Nette\Utils\HtmlDataset
+ *
+ * @author     Petr Morávek <petr@pada.cz>
+ */
+
+use Nette\Utils\HtmlDataset,
+	Tester\Assert;
+
+
+require __DIR__ . '/../bootstrap.php';
+
+
+test(function() {
+	$d = new HtmlDataset;
+	$d['string'] = 'string';
+	$d['empty'] = '';
+	$d['null'] = null;
+	$d['true'] = true;
+	$d['false'] = false;
+	$d['int'] = 42;
+	$d->list = array(1, 2);
+	$d->dict = array('a' => 'A', 1 => 2);
+	$obj = new \stdClass;
+	$obj->a = 'A';
+	$obj->b = 'B';
+	$d->obj = $obj;
+
+	Assert::same( 'data-string="string" data-empty="" data-true="true" data-false="false" data-int="42" data-list="[1,2]" data-dict=\'{"a":"A","1":2}\' data-obj=\'{"a":"A","b":"B"}\'', (string) $d );
+	Assert::same( 9, count($d) );
+	Assert::type( '\ArrayIterator', $d->getIterator() );
+});
+
+
+test(function() {
+	$d = new HtmlDataset(array('TestAttr' => false, 'testAttr' => true));
+	foreach (array('testAttr', 'test-attr', 'TestAttr', 'Test-Attr') as $name) {
+		Assert::true( isset($d->$name) );
+		Assert::true( $d->$name );
+
+		Assert::true( isset($d[$name]) );
+		Assert::true( $d[$name] );
+	}
+
+	Assert::false( isset($d->unknown) );
+	Assert::false( isset($d['unknown']) );
+});
+
+
+test(function() {
+	$d = new HtmlDataset(array('a-b' => true, 'c-d' => true));
+
+	Assert::true( isset($d->aB) );
+	Assert::true( isset($d['cD']) );
+
+	unset($d->aB);
+	unset($d['cD']);
+
+	Assert::false( isset($d->aB) );
+	Assert::false( isset($d['cD']) );
+});
