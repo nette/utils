cda96ff718e7bad5e949527dbfa1d6d0498181d9
David Grudl <david@grudl.com>
2015-03-17 13:28:47 +0100

    Strings::toAscii() uses //IGNORE

--- src/Utils/Strings.php
+++ src/Utils/Strings.php
@@ -182,7 +182,7 @@ class Strings
 				array("\xC2\xBB", "\xC2\xAB", "\xE2\x80\xA6", "\xE2\x84\xA2", "\xC2\xA9", "\xC2\xAE"),
 				array('>>', '<<', '...', 'TM', '(c)', '(R)'), $s
 			);
-			$s = @iconv('UTF-8', 'WINDOWS-1250//TRANSLIT', $s); // intentionally @
+			$s = @iconv('UTF-8', 'WINDOWS-1250//TRANSLIT//IGNORE', $s); // intentionally @
 			$s = strtr($s, "\xa5\xa3\xbc\x8c\xa7\x8a\xaa\x8d\x8f\x8e\xaf\xb9\xb3\xbe\x9c\x9a\xba\x9d\x9f\x9e"
 				. "\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3"
 				. "\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8"
@@ -191,7 +191,7 @@ class Strings
 				"ALLSSSSTZZZallssstzzzRAAAALCCCEEEEIIDDNNOOOOxRUUUUYTsraaaalccceeeeiiddnnooooruuuuyt- <->|-.");
 			$s = preg_replace('#[^\x00-\x7F]++#', '', $s);
 		} else {
-			$s = @iconv('UTF-8', 'ASCII//TRANSLIT', $s); // intentionally @
+			$s = @iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $s); // intentionally @
 		}
 		$s = str_replace(array('`', "'", '"', '^', '~', '?'), '', $s);
 		return strtr($s, "\x01\x02\x03\x04\x05\x06", '`\'"^~?');
--- tests/Utils/Strings.toAscii().phpt
+++ tests/Utils/Strings.toAscii().phpt
@@ -18,6 +18,7 @@ Assert::same( '"""\'\'\'>><<^', Strings::toAscii("\xE2\x80\x9E\xE2\x80\x9C\xE2\x
 Assert::same( '', Strings::toAscii("\xF0\x90\x80\x80") ); // U+10000
 Assert::same( '', Strings::toAscii("\xC2\xA4") ); // non-ASCII char
 Assert::same( 'a b', Strings::toAscii("a\xC2\xA0b") ); // non-breaking space
+Assert::same( 'Tarikh', Strings::toAscii("Ta\xCA\xBErikh") ); // Taʾrikh
 
 if (class_exists('Transliterator') && \Transliterator::create('Any-Latin; Latin-ASCII')) {
 	Assert::same( 'Athena->Moskva', Strings::toAscii("\xCE\x91\xCE\xB8\xCE\xAE\xCE\xBD\xCE\xB1\xE2\x86\x92\xD0\x9C\xD0\xBE\xD1\x81\xD0\xBA\xD0\xB2\xD0\xB0") ); // Αθήνα→Москва
