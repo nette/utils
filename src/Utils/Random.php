a7f756bc1e5fb2c3b42fdaada2fde879797174a4
David Grudl <david@grudl.com>
2015-06-30 14:44:23 +0200

    Random: /dev/urandom is not used on Windows

--- src/Utils/Random.php
+++ src/Utils/Random.php
@@ -39,7 +39,7 @@ class Random
 		if (empty($rand3) && function_exists('mcrypt_create_iv') && (PHP_VERSION_ID >= 50307 || !defined('PHP_WINDOWS_VERSION_BUILD'))) { // PHP bug #52523
 			$rand3 = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
 		}
-		if (empty($rand3) && @is_readable('/dev/urandom')) {
+		if (empty($rand3) && !defined('PHP_WINDOWS_VERSION_BUILD') && @is_readable('/dev/urandom')) {
 			$rand3 = file_get_contents('/dev/urandom', FALSE, NULL, -1, $length);
 		}
 		if (empty($rand3)) {
