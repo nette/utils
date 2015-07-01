d5eb0f40b74b961131c9c8962603276a1fd92a0f
David Grudl <david@grudl.com>
2015-07-01 12:19:22 +0200

    Callback::invokeSafe() workaround for HHVM bug https://github.com/facebook/hhvm/issues/4625

--- src/Utils/Callback.php
+++ src/Utils/Callback.php
@@ -84,7 +84,10 @@ class Callback
 	 */
 	public static function invokeSafe($function, array $args, $onError)
 	{
-		$prev = set_error_handler(function ($severity, $message, $file) use ($onError, & $prev, $function) {
+		$prev = set_error_handler(function ($severity, $message, $file, $line, $context = NULL, $stack = NULL) use ($onError, & $prev, $function) {
+			if ($file === '' && defined('HHVM_VERSION')) { // https://github.com/facebook/hhvm/issues/4625
+				$file = $stack[1]['file'];
+			}
 			if ($file === __FILE__ && $onError(str_replace("$function(): ", '', $message), $severity) !== FALSE) {
 				return;
 			} elseif ($prev) {
