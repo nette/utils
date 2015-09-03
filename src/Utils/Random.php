b2b1a8ecf9f1e2c0e75e8af1a6b396a44fd32854
David Grudl <david@grudl.com>
2015-09-03 12:20:08 +0200

    Random: skips microtime(), str_shuffle() and lcg_value() when cryptographically strong source is used

--- src/Utils/Random.php
+++ src/Utils/Random.php
@@ -26,38 +26,47 @@ class Random
 	 */
 	public static function generate($length = 10, $charlist = '0-9a-z')
 	{
-		$charlist = str_shuffle(preg_replace_callback('#.-.#', function ($m) {
+		if ($length === 0) {
+			return ''; // mcrypt_create_iv does not support zero length
+		}
+
+		$charlist = preg_replace_callback('#.-.#', function ($m) {
 			return implode('', range($m[0][0], $m[0][2]));
-		}, $charlist));
+		}, $charlist);
 		$chLen = strlen($charlist);
 
+		$windows = defined('PHP_WINDOWS_VERSION_BUILD');
+		$bytes = $res = '';
 		if (function_exists('openssl_random_pseudo_bytes')
 			&& (PHP_VERSION_ID >= 50400 || !defined('PHP_WINDOWS_VERSION_BUILD')) // slow in PHP 5.3 & Windows
 		) {
-			$rand3 = openssl_random_pseudo_bytes($length);
+			$bytes = openssl_random_pseudo_bytes($length);
 		}
-		if (empty($rand3) && function_exists('mcrypt_create_iv') && (PHP_VERSION_ID >= 50307 || !defined('PHP_WINDOWS_VERSION_BUILD'))) { // PHP bug #52523
-			$rand3 = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
+		if (strlen($bytes) < $length && function_exists('mcrypt_create_iv') && (PHP_VERSION_ID >= 50307 || !$windows)) { // PHP bug #52523
+			$bytes = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
 		}
-		if (empty($rand3) && !defined('PHP_WINDOWS_VERSION_BUILD') && @is_readable('/dev/urandom')) {
-			$rand3 = file_get_contents('/dev/urandom', FALSE, NULL, -1, $length);
+		if (strlen($bytes) < $length && !$windows && @is_readable('/dev/urandom')) {
+			$bytes = file_get_contents('/dev/urandom', FALSE, NULL, -1, $length);
 		}
-		if (empty($rand3)) {
-			static $cache;
-			$rand3 = $cache ?: $cache = md5(serialize($_SERVER), TRUE);
+		if (strlen($bytes) < $length) {
+			$rand3 = md5(serialize($_SERVER), TRUE);
+			$charlist = str_shuffle($charlist);
+			for ($i = 0; $i < $length; $i++) {
+				if ($i % 5 === 0) {
+					list($rand1, $rand2) = explode(' ', microtime());
+					$rand1 += lcg_value();
+				}
+				$rand1 *= $chLen;
+				$res .= $charlist[($rand1 + $rand2 + ord($rand3[$i % strlen($rand3)])) % $chLen];
+				$rand1 -= (int) $rand1;
+			}
+			return $res;
 		}
 
-		$s = '';
 		for ($i = 0; $i < $length; $i++) {
-			if ($i % 5 === 0) {
-				list($rand, $rand2) = explode(' ', microtime());
-				$rand += lcg_value();
-			}
-			$rand *= $chLen;
-			$s .= $charlist[($rand + $rand2 + ord($rand3[$i % strlen($rand3)])) % $chLen];
-			$rand -= (int) $rand;
+			$res .= $charlist[($i + ord($bytes[$i])) % $chLen];
 		}
-		return $s;
+		return $res;
 	}
 
 }
