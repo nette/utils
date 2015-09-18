da71611c06ec77ce50deb8067eb876eccf6fb433
David Grudl <david@grudl.com>
2015-09-18 16:06:14 +0200

    tests: removes temp dir after test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,9 +15,12 @@ date_default_timezone_set('Europe/Prague');
 
 
 // create temporary directory
-define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
-@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
-Tester\Helpers::purge(TEMP_DIR);
+define('TEMP_DIR', __DIR__ . '/tmp/' . lcg_value());
+@mkdir(TEMP_DIR, 0777, TRUE); // @ - base directory may already exist
+register_shutdown_function(function () {
+	Tester\Helpers::purge(TEMP_DIR);
+	rmdir(TEMP_DIR);
+});
 
 
 function before(\Closure $function = NULL)
