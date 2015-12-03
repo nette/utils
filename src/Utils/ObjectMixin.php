6f48d69b24a84c92bf4ef2179c0dc8647220c5a0
David Grudl <david@grudl.com>
2015-12-03 02:32:27 +0100

    ObjectMixin: added getSuggestion for forward compatibility

--- src/Utils/ObjectMixin.php
+++ src/Utils/ObjectMixin.php
@@ -395,4 +395,27 @@ class ObjectMixin
 		return $cache = FALSE;
 	}
 
+
+	/**
+	 * Finds the best suggestion (for 8-bit encoding).
+	 * @return string|NULL
+	 * @internal
+	 */
+	public static function getSuggestion(array $items, $value)
+	{
+		$norm = preg_replace($re = '#^(get|set|has|is|add)(?=[A-Z])#', '', $value);
+		$best = NULL;
+		$min = (strlen($value) / 4 + 1) * 10 + .1;
+		foreach (array_unique($items) as $item) {
+			if ($item !== $value && (
+				($len = levenshtein($item, $value, 10, 11, 10)) < $min
+				|| ($len = levenshtein(preg_replace($re, '', $item), $norm, 10, 11, 10) + 20) < $min
+			)) {
+				$min = $len;
+				$best = $item;
+			}
+		}
+		return $best;
+	}
+
 }
