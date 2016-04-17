c7aad62b45a4eda8d1916e14c8040000a011cea7
David Grudl <david@grudl.com>
2016-04-17 23:38:55 +0200

    Object: removed alias for ObjectMixin, it may interfere with nette/deprecated, which creates alias Nette\ObjectMixin

--- src/Utils/Object.php
+++ src/Utils/Object.php
@@ -8,7 +8,6 @@
 namespace Nette;
 
 use Nette;
-use Nette\Utils\ObjectMixin;
 
 
 /**
@@ -72,7 +71,7 @@ abstract class Object
 	 */
 	public function __call($name, $args)
 	{
-		return ObjectMixin::call($this, $name, $args);
+		return Nette\Utils\ObjectMixin::call($this, $name, $args);
 	}
 
 
@@ -85,7 +84,7 @@ abstract class Object
 	 */
 	public static function __callStatic($name, $args)
 	{
-		return ObjectMixin::callStatic(get_called_class(), $name, $args);
+		return Nette\Utils\ObjectMixin::callStatic(get_called_class(), $name, $args);
 	}
 
 
@@ -104,9 +103,9 @@ abstract class Object
 			$class = (new \ReflectionClass($class))->getName();
 		}
 		if ($callback === NULL) {
-			return ObjectMixin::getExtensionMethod($class, $name);
+			return Nette\Utils\ObjectMixin::getExtensionMethod($class, $name);
 		} else {
-			ObjectMixin::setExtensionMethod($class, $name, $callback);
+			Nette\Utils\ObjectMixin::setExtensionMethod($class, $name, $callback);
 		}
 	}
 
@@ -119,7 +118,7 @@ abstract class Object
 	 */
 	public function &__get($name)
 	{
-		return ObjectMixin::get($this, $name);
+		return Nette\Utils\ObjectMixin::get($this, $name);
 	}
 
 
@@ -132,7 +131,7 @@ abstract class Object
 	 */
 	public function __set($name, $value)
 	{
-		ObjectMixin::set($this, $name, $value);
+		Nette\Utils\ObjectMixin::set($this, $name, $value);
 	}
 
 
@@ -143,7 +142,7 @@ abstract class Object
 	 */
 	public function __isset($name)
 	{
-		return ObjectMixin::has($this, $name);
+		return Nette\Utils\ObjectMixin::has($this, $name);
 	}
 
 
@@ -155,7 +154,7 @@ abstract class Object
 	 */
 	public function __unset($name)
 	{
-		ObjectMixin::remove($this, $name);
+		Nette\Utils\ObjectMixin::remove($this, $name);
 	}
 
 }
