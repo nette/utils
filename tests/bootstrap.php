6a542359ef2be90ba6ed0883288c80fd36320f7b
David Grudl <david@grudl.com>
2017-01-26 02:51:23 +0100

    tests: fixed mkdir() race condition

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,11 +16,8 @@ date_default_timezone_set('Europe/Prague');
 
 // create temporary directory
 define('TEMP_DIR', __DIR__ . '/tmp/' . lcg_value());
-@mkdir(TEMP_DIR, 0777, TRUE); // @ - base directory may already exist
-register_shutdown_function(function () {
-	Tester\Helpers::purge(TEMP_DIR);
-	rmdir(TEMP_DIR);
-});
+@mkdir(dirname(TEMP_DIR));
+@mkdir(TEMP_DIR);
 
 
 function test(\Closure $function)
