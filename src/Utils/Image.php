6d5f0c9f5d2242fc9ce7788044ea76a006e0d402
Jiří Svěcený <sveceny@sitole.cz>
2018-03-23 17:48:32 +0100

    Utils: added support for auto create folder if you save image into no-exists directory

--- src/Utils/Image.php
+++ src/Utils/Image.php
@@ -492,7 +492,11 @@ class Image
 			$type = $extensions[$ext];
 		}
 
-		switch ($type) {
+        if (array_key_exists($type, self::$formats) && $file != null) {
+            FileSystem::createDir(dirname($file));
+        }
+
+        switch ($type) {
 			case self::JPEG:
 				$quality = $quality === null ? 85 : max(0, min(100, $quality));
 				$success = imagejpeg($this->image, $file, $quality);
