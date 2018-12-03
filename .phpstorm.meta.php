f5337dae7d14995b7adfdc48a1dd14165dd166fe
David Grudl <david@grudl.com>
2018-12-03 20:12:54 +0100

    added PhpStorm metadata for autocompletion

---
+++ .phpstorm.meta.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace PHPSTORM_META;
+
+override(\Nette\Utils\Arrays::get(0), elementType(0));
+override(\Nette\Utils\Arrays::getRef(0), elementType(0));
+override(\Nette\Utils\Arrays::grep(0), type(0));
