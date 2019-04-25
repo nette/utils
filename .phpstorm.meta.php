9f9fadd7158cfff8784fdd802a0c6dfced2a8fd5
David Grudl <david@grudl.com>
2019-04-25 20:32:50 +0200

    updated .phpstorm.meta.php
    (is not yet supported by PhpStorm)

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -7,6 +7,7 @@ namespace PHPSTORM_META;
 override(\Nette\Utils\Arrays::get(0), elementType(0));
 override(\Nette\Utils\Arrays::getRef(0), elementType(0));
 override(\Nette\Utils\Arrays::grep(0), type(0));
+override(\Nette\Utils\Arrays::toObject(0), type(1));
 
 expectedArguments(\Nette\Utils\Arrays::grep(), 2, PREG_GREP_INVERT);
 expectedArguments(\Nette\Utils\Image::resize(), 2, \Nette\Utils\Image::SHRINK_ONLY, \Nette\Utils\Image::STRETCH, \Nette\Utils\Image::FIT, \Nette\Utils\Image::FILL, \Nette\Utils\Image::EXACT);
