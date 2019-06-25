033486c04094f8d93e956c6ead033fa868a676ae
Jan Barášek <janbarasek@gmail.com>
2019-06-25 22:46:51 +0200

    Validators: improved cs

--- src/Utils/Validators.php
+++ src/Utils/Validators.php
@@ -199,7 +199,7 @@ class Validators
 	 */
 	public static function isNumericInt($value): bool
 	{
-		return is_int($value) || is_string($value) && preg_match('#^[+-]?[0-9]+\z#', $value);
+		return is_int($value) || (is_string($value) && preg_match('#^[+-]?[0-9]+\z#', $value));
 	}
 
 
@@ -208,7 +208,7 @@ class Validators
 	 */
 	public static function isNumeric($value): bool
 	{
-		return is_float($value) || is_int($value) || is_string($value) && preg_match('#^[+-]?[0-9]*[.]?[0-9]+\z#', $value);
+		return is_float($value) || is_int($value) || (is_string($value) && preg_match('#^[+-]?[0-9]*[.]?[0-9]+\z#', $value));
 	}
 
 
