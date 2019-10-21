13699ddc1c9926f346ff1a9444ad33310f14c8ae
David Grudl <david@grudl.com>
2019-10-21 21:32:14 +0200

    Json: added ESCAPE_UNICODE

--- src/Utils/Json.php
+++ src/Utils/Json.php
@@ -23,13 +23,16 @@ final class Json
 
 	public const PRETTY = 0b0010;
 
+	public const ESCAPE_UNICODE = 0b0100;
+
 
 	/**
 	 * Returns the JSON representation of a value. Accepts flag Json::PRETTY.
 	 */
 	public static function encode($value, int $flags = 0): string
 	{
-		$flags = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+		$flags = ($flags & self::ESCAPE_UNICODE ? 0 : JSON_UNESCAPED_UNICODE)
+			| JSON_UNESCAPED_SLASHES
 			| ($flags & self::PRETTY ? JSON_PRETTY_PRINT : 0)
 			| (defined('JSON_PRESERVE_ZERO_FRACTION') ? JSON_PRESERVE_ZERO_FRACTION : 0); // since PHP 5.6.6 & PECL JSON-C 1.3.7
 
