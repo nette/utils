e4ffbcbb3921c2d445706000946ad19870438ee0
David Grudl <david@grudl.com>
2019-12-28 01:31:52 +0100

    ObjectHelpers::getSuggestion() improved weights
    Better counts with method's prefix change.

--- src/Utils/ObjectHelpers.php
+++ src/Utils/ObjectHelpers.php
@@ -131,14 +131,14 @@ final class ObjectHelpers
 	 */
 	public static function getSuggestion(array $possibilities, string $value): ?string
 	{
-		$norm = preg_replace($re = '#^(get|set|has|is|add)(?=[A-Z])#', '', $value);
+		$norm = preg_replace($re = '#^(get|set|has|is|add)(?=[A-Z])#', '+', $value);
 		$best = null;
 		$min = (strlen($value) / 4 + 1) * 10 + .1;
 		foreach (array_unique($possibilities, SORT_REGULAR) as $item) {
 			$item = $item instanceof \Reflector ? $item->getName() : $item;
 			if ($item !== $value && (
 				($len = levenshtein($item, $value, 10, 11, 10)) < $min
-				|| ($len = levenshtein(preg_replace($re, '', $item), $norm, 10, 11, 10) + 20) < $min
+				|| ($len = levenshtein(preg_replace($re, '*', $item), $norm, 10, 11, 10)) < $min
 			)) {
 				$min = $len;
 				$best = $item;
