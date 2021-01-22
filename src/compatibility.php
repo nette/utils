5f92ee6c18ae1323686490c7d25d03186926dc39
David Grudl <david@grudl.com>
2021-01-22 23:58:44 +0100

    compatibility: improved code hinting

--- src/compatibility.php
+++ src/compatibility.php
@@ -7,24 +7,26 @@
 
 declare(strict_types=1);
 
-namespace Nette\Utils {
-	if (false) {
-		/** @deprecated use Nette\HtmlStringable */
-		interface IHtmlString
-		{
-		}
-	} elseif (!interface_exists(IHtmlString::class)) {
-		class_alias(\Nette\HtmlStringable::class, IHtmlString::class);
+namespace Nette\Utils;
+
+use Nette;
+
+if (false) {
+	/** @deprecated use Nette\HtmlStringable */
+	interface IHtmlString extends Nette\HtmlStringable
+	{
 	}
+} elseif (!interface_exists(IHtmlString::class)) {
+	class_alias(Nette\HtmlStringable::class, IHtmlString::class);
 }
 
-namespace Nette\Localization {
-	if (false) {
-		/** @deprecated use Nette\Localization\Translator */
-		interface ITranslator
-		{
-		}
-	} elseif (!interface_exists(ITranslator::class)) {
-		class_alias(Translator::class, ITranslator::class);
+namespace Nette\Localization;
+
+if (false) {
+	/** @deprecated use Nette\Localization\Translator */
+	interface ITranslator extends Translator
+	{
 	}
+} elseif (!interface_exists(ITranslator::class)) {
+	class_alias(Translator::class, ITranslator::class);
 }
