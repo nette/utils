5b6cccd9abefc786244f9537f609e598a099b926
David Grudl <david@grudl.com>
2021-03-01 19:15:56 +0100

    added property typehints

--- src/Utils/Image.php
+++ src/Utils/Image.php
@@ -126,8 +126,7 @@ class Image
 
 	private const FORMATS = [self::JPEG => 'jpeg', self::PNG => 'png', self::GIF => 'gif', self::WEBP => 'webp', self::AVIF => 'avif', self::BMP => 'bmp'];
 
-	/** @var resource|\GdImage */
-	private $image;
+	private \GdImage $image;
 
 
 	/**
