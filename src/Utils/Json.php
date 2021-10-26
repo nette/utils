5c2c477f784f65a4ca08a8de61e519153a15a7b2
David Grudl <david@grudl.com>
2021-10-27 00:25:02 +0200

    Strings, Arrays, Json, Image: $flags replaced with parameters

--- src/Utils/Json.php
+++ src/Utils/Json.php
@@ -19,23 +19,32 @@ final class Json
 {
 	use Nette\StaticClass;
 
+	/** @deprecated */
 	public const FORCE_ARRAY = 0b0001;
 
+	/** @deprecated */
 	public const PRETTY = 0b0010;
 
+	/** @deprecated */
 	public const ESCAPE_UNICODE = 0b0100;
 
 
 	/**
-	 * Converts value to JSON format. The flag can be Json::PRETTY, which formats JSON for easier reading and clarity,
-	 * and Json::ESCAPE_UNICODE for ASCII output.
+	 * Converts value to JSON format. Use $pretty for easier reading and clarity and $escapeUnicode for ASCII output.
 	 * @throws JsonException
 	 */
-	public static function encode(mixed $value, int $flags = 0): string
-	{
-		$flags = ($flags & self::ESCAPE_UNICODE ? 0 : JSON_UNESCAPED_UNICODE)
+	public static function encode(
+		mixed $value,
+		bool|int $pretty = false,
+		bool $escapeUnicode = false,
+	): string {
+		if (is_int($pretty)) { // back compatibility
+			$escapeUnicode = $pretty & self::ESCAPE_UNICODE;
+			$pretty &= self::PRETTY;
+		}
+		$flags = ($escapeUnicode ? 0 : JSON_UNESCAPED_UNICODE)
 			| JSON_UNESCAPED_SLASHES
-			| ($flags & self::PRETTY ? JSON_PRETTY_PRINT : 0)
+			| ($pretty ? JSON_PRETTY_PRINT : 0)
 			| (defined('JSON_PRESERVE_ZERO_FRACTION') ? JSON_PRESERVE_ZERO_FRACTION : 0); // since PHP 5.6.6 & PECL JSON-C 1.3.7
 
 		$json = json_encode($value, $flags);
@@ -47,13 +56,12 @@ final class Json
 
 
 	/**
-	 * Parses JSON to PHP value. The flag can be Json::FORCE_ARRAY, which forces an array instead of an object as the return value.
+	 * Parses JSON to PHP value. Parameter $forceArray forces an array instead of an object as the return value.
 	 * @throws JsonException
 	 */
-	public static function decode(string $json, int $flags = 0): mixed
+	public static function decode(string $json, bool|int $forceArray = false): mixed
 	{
-		$forceArray = (bool) ($flags & self::FORCE_ARRAY);
-		$value = json_decode($json, $forceArray, 512, JSON_BIGINT_AS_STRING);
+		$value = json_decode($json, (bool) $forceArray, 512, JSON_BIGINT_AS_STRING);
 		if ($error = json_last_error()) {
 			throw new JsonException(json_last_error_msg(), $error);
 		}
