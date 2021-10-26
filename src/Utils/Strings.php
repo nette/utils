6619f590c3957a232f4d900fe04770ccbc83d7a1
David Grudl <david@grudl.com>
2021-10-27 01:21:23 +0200

    Strings: added support for UTF8 offsets in regexp

--- src/Utils/Strings.php
+++ src/Utils/Strings.php
@@ -490,11 +490,18 @@ class Strings
 		bool|int $captureOffset = false,
 		bool $skipEmpty = false,
 		int $limit = -1,
+		bool $utf8 = false,
 	): array {
 		$flags = is_int($captureOffset) && $captureOffset // back compatibility
 			? $captureOffset
 			: ($captureOffset ? PREG_SPLIT_OFFSET_CAPTURE : 0) | ($skipEmpty ? PREG_SPLIT_NO_EMPTY : 0);
-		return self::pcre('preg_split', [$pattern, $subject, $limit, $flags | PREG_SPLIT_DELIM_CAPTURE]);
+		$pattern .= $utf8 ? 'u' : '';
+		$m = self::pcre('preg_split', [$pattern, $subject, $limit, $flags | PREG_SPLIT_DELIM_CAPTURE]);
+		if ($utf8 && ($flags & PREG_SPLIT_OFFSET_CAPTURE)) {
+			return self::bytesToChars($subject, [$m])[0];
+		}
+
+		return $m;
 	}
 
 
@@ -507,17 +514,29 @@ class Strings
 		bool|int $captureOffset = false,
 		int $offset = 0,
 		bool $unmatchedAsNull = false,
+		bool $utf8 = false,
 	): ?array {
 		$flags = is_int($captureOffset) && $captureOffset // back compatibility
 			? $captureOffset
 			: ($captureOffset ? PREG_OFFSET_CAPTURE : 0) | ($unmatchedAsNull ? PREG_UNMATCHED_AS_NULL : 0);
+		if ($utf8) {
+			$offset = strlen(self::substring($subject, 0, $offset));
+			$pattern .= 'u';
+		}
+
 		if ($offset > strlen($subject)) {
 			return null;
 		}
 
-		return self::pcre('preg_match', [$pattern, $subject, &$m, $flags, $offset])
-			? $m
-			: null;
+		if (!self::pcre('preg_match', [$pattern, $subject, &$m, $flags, $offset])) {
+			return null;
+		}
+
+		if ($utf8 && ($flags & PREG_OFFSET_CAPTURE)) {
+			return self::bytesToChars($subject, [$m])[0];
+		}
+
+		return $m;
 	}
 
 
@@ -532,10 +551,16 @@ class Strings
 		int $offset = 0,
 		bool $unmatchedAsNull = false,
 		bool $patternOrder = false,
+		bool $utf8 = false,
 	): array {
 		$flags = is_int($captureOffset) && $captureOffset // back compatibility
 			? $captureOffset
 			: ($captureOffset ? PREG_OFFSET_CAPTURE : 0) | ($unmatchedAsNull ? PREG_UNMATCHED_AS_NULL : 0) | ($patternOrder ? PREG_PATTERN_ORDER : 0);
+		if ($utf8) {
+			$offset = strlen(self::substring($subject, 0, $offset));
+			$pattern .= 'u';
+		}
+
 		if ($offset > strlen($subject)) {
 			return [];
 		}
@@ -545,6 +570,10 @@ class Strings
 			($flags & PREG_PATTERN_ORDER) ? $flags : ($flags | PREG_SET_ORDER),
 			$offset,
 		]);
+		if ($utf8 && ($flags & PREG_OFFSET_CAPTURE)) {
+			return self::bytesToChars($subject, $m);
+		}
+
 		return $m;
 	}
 
@@ -559,6 +588,7 @@ class Strings
 		int $limit = -1,
 		bool $captureOffset = false,
 		bool $unmatchedAsNull = false,
+		bool $utf8 = false,
 	): string {
 		if (is_object($replacement) || is_array($replacement)) {
 			if (!is_callable($replacement, false, $textual)) {
@@ -566,6 +596,13 @@ class Strings
 			}
 
 			$flags = ($captureOffset ? PREG_OFFSET_CAPTURE : 0) | ($unmatchedAsNull ? PREG_UNMATCHED_AS_NULL : 0);
+			if ($utf8) {
+				$pattern .= 'u';
+				if ($captureOffset) {
+					$replacement = fn($m) => $replacement(self::bytesToChars($subject, [$m])[0]);
+				}
+			}
+
 			return self::pcre('preg_replace_callback', [$pattern, $replacement, $subject, $limit, 0, $flags]);
 
 		} elseif (is_array($pattern) && is_string(key($pattern))) {
@@ -573,7 +610,31 @@ class Strings
 			$pattern = array_keys($pattern);
 		}
 
+		if ($utf8) {
+			$pattern = array_map(fn($item) => $item . 'u', (array) $pattern);
+		}
+
 		return self::pcre('preg_replace', [$pattern, $replacement, $subject, $limit]);
+	}
+
+
+	private static function bytesToChars(string $s, array $groups): array
+	{
+		$lastBytes = $lastChars = 0;
+		foreach ($groups as &$matches) {
+			foreach ($matches as &$match) {
+				if ($match[1] > $lastBytes) {
+					$lastChars += self::length(substr($s, $lastBytes, $match[1] - $lastBytes));
+				} elseif ($match[1] < $lastBytes) {
+					$lastChars -= self::length(substr($s, $match[1], $lastBytes - $match[1]));
+				}
+
+				$lastBytes = $match[1];
+				$match[1] = $lastChars;
+			}
+		}
+
+		return $groups;
 	}
 
 
--- tests/Utils/Strings.split().phpt
+++ tests/Utils/Strings.split().phpt
@@ -46,11 +46,29 @@ Assert::same([
 ], Strings::split('a, b, c', '#(,)\s*#', PREG_SPLIT_OFFSET_CAPTURE));
 
 Assert::same([
-	['a', 0],
-	[',', 1],
-	['b', 3],
-	[',', 4],
-	['c', 6],
-], Strings::split('a, b, c', '#(,)\s*#', captureOffset: true));
+	['ž', 0],
+	['lu', 2],
+	['ť', 4],
+	['ou', 6],
+	['č', 8],
+	['k', 10],
+	['ý ', 11],
+	['k', 14],
+	['ůň', 15],
+], Strings::split('žluťoučký kůň', '#([a-z]+)\s*#u', captureOffset: true));
+
+Assert::same([
+	['ž', 0],
+	['lu', 1],
+	['ť', 3],
+	['ou', 4],
+	['č', 6],
+	['k', 7],
+	['ý ', 8],
+	['k', 10],
+	['ůň', 11],
+], Strings::split('žluťoučký kůň', '#([a-z]+)\s*#u', captureOffset: true, utf8: true));
+
+Assert::same(['', ' ', ''], Strings::split('žluťoučký kůň', '#\w+#', utf8: true)); // without modifier
 
 Assert::same(['a', ',', 'b, c'], Strings::split('a, b, c', '#(,)\s*#', limit: 2));
