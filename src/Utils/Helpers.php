8ebc53f57e8651419f1411c9055a76de29182782
David Grudl <david@grudl.com>
2022-01-05 02:00:27 +0100

    Helpers: fixed exception

--- src/Utils/Helpers.php
+++ src/Utils/Helpers.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 
 namespace Nette\Utils;
 
+use Nette;
+
 
 class Helpers
 {
@@ -62,7 +64,7 @@ class Helpers
 	public static function clamp($value, $min, $max)
 	{
 		if ($min > $max) {
-			throw new \InvalidArgumentException("Minimum ($min) is not less than maximum ($max).");
+			throw new Nette\InvalidArgumentException("Minimum ($min) is not less than maximum ($max).");
 		}
 
 		return min(max($value, $min), $max);
