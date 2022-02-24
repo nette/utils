6830fc22a76febf7f695a535f37a988832e9225d
David Grudl <david@grudl.com>
2022-02-24 07:05:24 +0100

    RegexpException: uses preg_last_error_msg()

--- src/Utils/Strings.php
+++ src/Utils/Strings.php
@@ -677,7 +677,7 @@ class Strings
 		if (($code = preg_last_error()) // run-time error, but preg_last_error & return code are liars
 			&& ($res === null || !in_array($func, ['preg_filter', 'preg_replace_callback', 'preg_replace'], true))
 		) {
-			throw new RegexpException((RegexpException::MESSAGES[$code] ?? 'Unknown error')
+			throw new RegexpException(preg_last_error_msg()
 				. ' (pattern: ' . implode(' or ', (array) $args[0]) . ')', $code);
 		}
 
