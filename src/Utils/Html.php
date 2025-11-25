baa7cb7cb24338cc015229b207573c8cdc3388a3
David Grudl <david@grudl.com>
2025-11-25 13:35:14 +0100

    Html: improved type hints [Closes nette/nette#1570]

--- src/Utils/Html.php
+++ src/Utils/Html.php
@@ -234,17 +234,17 @@ use const ENT_HTML5, ENT_NOQUOTES, ENT_QUOTES;
 class Html implements \ArrayAccess, \Countable, \IteratorAggregate, HtmlStringable
 {
 	/** @var array<string, mixed>  element's attributes */
-	public $attrs = [];
+	public array $attrs = [];
 
 	/** void elements */
-	public static $emptyElements = [
+	public static array $emptyElements = [
 		'img' => 1, 'hr' => 1, 'br' => 1, 'input' => 1, 'meta' => 1, 'area' => 1, 'embed' => 1, 'keygen' => 1,
 		'source' => 1, 'base' => 1, 'col' => 1, 'link' => 1, 'param' => 1, 'basefont' => 1, 'frame' => 1,
 		'isindex' => 1, 'wbr' => 1, 'command' => 1, 'track' => 1,
 	];
 
 	/** @var array<int, HtmlStringable|string> nodes */
-	protected $children = [];
+	protected array $children = [];
 
 	/** element's name */
 	private string $name = '';
@@ -573,7 +573,7 @@ class Html implements \ArrayAccess, \Countable, \IteratorAggregate, HtmlStringab
 	/**
 	 * Adds new element's child.
 	 */
-	final public function addHtml(mixed $child): static
+	final public function addHtml(HtmlStringable|string $child): static
 	{
 		return $this->insert(null, $child);
 	}
@@ -582,7 +582,7 @@ class Html implements \ArrayAccess, \Countable, \IteratorAggregate, HtmlStringab
 	/**
 	 * Appends plain-text string to element content.
 	 */
-	public function addText(mixed $text): static
+	public function addText(\Stringable|string $text): static
 	{
 		if (!$text instanceof HtmlStringable) {
 			$text = htmlspecialchars((string) $text, ENT_NOQUOTES, 'UTF-8');
